55e9bf16fbcc3e2cb212cd9cf1aa234d6c2508b4
dssp100 <dssp@freenet.de>
2017-01-18 20:17:40 -0400

    Initial Commit

---
+++ ToDo MasterTests/ToDo_MasterTests.swift
@@ -0,0 +1,36 @@
+//
+//  ToDo_MasterTests.swift
+//  ToDo MasterTests
+//
+//  Created by Daniel Springer on 18/01/2017.
+//  Copyright © 2017 Daniel Springer. All rights reserved.
+//
+
+import XCTest
+@testable import ToDo_Master
+
+class ToDo_MasterTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
